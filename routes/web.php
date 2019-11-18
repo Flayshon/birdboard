60e7c2394cb7dc8df8ca8018c21b7c19a3981777
flayshon <flayshon@gmail.com>
2019-11-18 13:50:24 -0300

    pre-tailwind configuration stage

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/projects', 'ProjectsController@index')->middleware('auth');
+    Route::get('/projects/{project}', 'ProjectsController@show')->middleware('auth');
+    Route::post('/projects', 'ProjectsController@store')->middleware('auth');    
+    
+    Route::get('/home', 'HomeController@index')->name('home');
+});    
+
