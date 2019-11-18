2c8ca396698c5f37dbed38de82d548c41d9a2373
flayshon <flayshon@gmail.com>
2019-11-18 14:36:21 -0300

    prepared the views before installing tailwind

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel</title>
+</head>
+<body>
+    <h1>Create a project</h1>
+
+    <form method="POST" action="/projects">
+        @csrf
+        
+        <div class="field">
+            <label for="title" class="label">Title</label>
+
+            <div class="control">
+                <input type="text" class="input" name="title" placeholder="Title">
+            </div>
+        </div>
+
+        <div class="field">
+            <label for="description" class="label">description</label>
+
+            <div class="control">
+                <textarea name="description" class="textarea" ></textarea>
+            </div>
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-link">Create Project</button>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -6,47 +6,40 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ProjectsTest extends TestCase
+class ManageProjectsTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
     
     /** @test */
-    public function guests_cannot_create_projects()
-    {
-        $attributes = factory('App\User')->raw();
-    
-        $this->post('/projects', $attributes)->assertRedirect('login');
-    }
-    
-    /** @test */
-    public function guests_cannot_view_projects()
-    {
-        $this->get('/projects')->assertRedirect('login');
-    }
-    
-    /** @test */
-    public function guests_cannot_view_a_single_project()
+    public function guests_cannot_manage_projects()
     {
         $project = factory('App\Project')->create();
         
+        $this->get('/projects')->assertRedirect('login');
+        $this->get('/projects/create')->assertRedirect('login');
         $this->get($project->path())->assertRedirect('login');
+        $this->post('/projects', $project->toArray())->assertRedirect('login');
     }
     
     /** @test */
     public function a_user_can_create_a_project()
     {
+        $this->withoutExceptionHandling();
+
         $this->actingAs(factory('App\User')->create());
         
-        $attributes = [
+        $project = [
             'title' => $this->faker->sentence,
             'description' => $this->faker->paragraph
         ];
+
+        $this->get('/projects/create')->assertStatus(200);
         
-        $this->post('/projects', $attributes)->assertRedirect('/projects');
+        $this->post('/projects', $project)->assertRedirect('/projects');
 
-        $this->assertDatabaseHas('projects', $attributes);
+        $this->assertDatabaseHas('projects', $project);
 
-        $this->get('/projects')->assertSee($attributes['title']);
+        $this->get('/projects')->assertSee($project['title']);
     }
 
     /** @test */
@@ -54,9 +47,9 @@ class ProjectsTest extends TestCase
     {
         $this->actingAs(factory('App\User')->create());
         
-        $attributes = factory('App\Project')->raw(['title' => '']);
+        $project = factory('App\Project')->raw(['title' => '']);
 
-        $this->post('/projects', $attributes)->assertSessionHasErrors();
+        $this->post('/projects', $project)->assertSessionHasErrors();
     }
     
     /** @test */
@@ -64,9 +57,9 @@ class ProjectsTest extends TestCase
     {
         $this->actingAs(factory('App\User')->create());
         
-        $attributes = factory('App\Project')->raw(['description' => '']);
+        $project = factory('App\Project')->raw(['description' => '']);
 
-        $this->post('/projects', $attributes)->assertSessionHasErrors();
+        $this->post('/projects', $project)->assertSessionHasErrors();
     }
     
     /** @test */
